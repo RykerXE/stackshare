377ae7139fbed42f4db0f8f598baf515f4e0b6e6
Ryker <rajnish.kumar@byjus.com>
2020-07-22 16:54:24 +0530

    Implemented socket

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const http = require("http");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const pino = require("pino");
+const socketIo = require("socket.io");
 require("dotenv").config();
 
+const Shares = require("./src/models/Shares");
 const apiRoutes = require("./src/routes");
 
 const { PORT, DB_URI, NODE_ENV } = process.env;
@@ -45,4 +48,34 @@ app.get(`/healthcheck`, (req, res) => {
 });
 app.use(`/api`, apiRoutes);
 
-app.listen(PORT, () => logger.info(`Application Server started and running on port ${PORT}`));
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let interval;
+
+const emitSharesList = async socket => {
+    const options = {
+        page:  1,
+        limit: 10,
+    };
+    const aggregate = Shares.aggregate();
+    const data = await Shares.aggregatePaginate(aggregate, options);
+    socket.emit("sharesList", data);
+  }
+
+io.on("connection", (socket) => {
+    logger.info("New client connected" + socket.id);
+
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => emitSharesList(socket), 3000);
+
+    socket.on("disconnect", () => {
+        logger.info("Client disconnected");
+        clearInterval(interval);
+    });
+});
+
+server.listen(PORT, () => logger.info(`Listening on PORT ${PORT}`));
+//app.listen(PORT, () => logger.info(`Application Server started and running on port ${PORT}`));
